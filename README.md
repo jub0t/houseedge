d71c0c695a5cc3ea3ec2a83a7914a2a4e8957c99
Jareer <jforeverything2007@gmail.com>
2024-05-20 22:34:09 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# HouseEdge
+# House Edge - Researching Gambling Technology Using Statistics
+
+- Terminologies
+    - `Vec<T>` - A Vector/Array/List in Rustlang of type `T`, or simply `Vec`.
--- src/core/vector.rs
+++ src/core/vector.rs
@@ -1,5 +1,6 @@
 use rand::seq::SliceRandom;
 
+// Pseudo-random Shuffling
 pub fn shuffle_vector<T>(items: &mut Vec<T>) -> &Vec<T> {
     let mut rng = rand::thread_rng();
     items.shuffle(&mut rng);
